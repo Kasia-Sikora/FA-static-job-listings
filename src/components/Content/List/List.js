3ada2a8886a12025b94d5bcdac31e1a3b47b31a6
Kasia <sikora.katarzyna.waw@gmail.com>
2021-03-16 19:23:29 +0100

    Refactor App and Content components

--- src/components/Content/List/List.js
+++ src/components/Content/List/List.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Content.module.scss";
+import styles from "./../Content.module.scss";
 import ListItem from "./ListItem";
 
 const List = ({ filteredJobs, searchAttrHandler}) => {
